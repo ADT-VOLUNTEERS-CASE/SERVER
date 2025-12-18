b6ce7fb9588005352ffccd4792833ef856cc3f06
TimofeyNov <timn2020@inbox.ru>
2025-12-18 19:52:07 +0300

    feat: add swagger and application.properties and pom.xml

---
+++ src/main/java/org/adt/volunteerscase/VolunteersCaseApplication.java
@@ -0,0 +1,17 @@
+package org.adt.volunteerscase;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteersCaseApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
+        SpringApplication.run(VolunteersCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/adt/volunteerscase/VolunteersCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adt.volunteerscase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteersCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
