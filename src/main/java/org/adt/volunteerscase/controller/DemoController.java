1a99701738dde87f8cf38b09225041206e4e4ac8
TimofeyNov <timn2020@inbox.ru>
2026-01-14 00:33:07 +0300

    fix: transfer @SecurityRequirement(name = jwtAuth) in DemoController

--- src/main/java/org/adt/volunteerscase/controller/DemoController.java
+++ src/main/java/org/adt/volunteerscase/controller/DemoController.java
@@ -2,16 +2,13 @@ package org.adt.volunteerscase.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1")
-@SecurityRequirement(name = "jwtAuth")
 public class DemoController {
     /**
      * Responds to a GET request with a simple "pong" message.
@@ -30,6 +27,7 @@ public class DemoController {
     @Operation(
             summary = "ping pong with auth"
     )
+    @SecurityRequirement(name = "jwtAuth")
     @GetMapping("/authping")
     public ResponseEntity<String> authping() {
         return ResponseEntity.ok("pong");
