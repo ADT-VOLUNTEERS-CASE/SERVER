95c5e663c3dfe935640a012b0c55e7c977fa9a6e
TimofeyNovv <timn2020@inbox.ru>
2026-03-06 15:41:47 +0300

    Fix: Apply swagger config to use https scheme (#21)
    * add path for swagger(/docs)
    
    * fix(swagger): fix CORS problem

--- src/main/java/org/adt/volunteerscase/config/SwaggerConfig.java
+++ src/main/java/org/adt/volunteerscase/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -18,6 +19,10 @@ import org.springframework.context.annotation.Configuration;
                 title = "Volunteers API",
                 version = "1.0",
                 description = "documentation for backend endpoints"
-        )
+        ),
+        servers = {
+                @Server(url = "https://adt.rss14.ru/", description = "Production"),
+                @Server(url = "http://localhost:8080/", description = "Dev")
+        }
 )
 public class SwaggerConfig {}
