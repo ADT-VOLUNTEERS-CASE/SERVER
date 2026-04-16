985dcf7215cd1aa6ff57ddceca5f4edcecb75cd2
TimofeyNov <timn2020@inbox.ru>
2026-04-16 14:53:02 +0300

    fix

--- src/test/java/org/adt/volunteerscase/VolunteersCaseApplicationTests.java
+++ src/test/java/org/adt/volunteerscase/VolunteersCaseApplicationTests.java
@@ -1,20 +1,13 @@
 package org.adt.volunteerscase;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+@SpringBootTest
 class VolunteersCaseApplicationTests {
 
     @Test
-    void mainMethod_shouldBePresentAndStatic() throws NoSuchMethodException {
-        Method mainMethod = VolunteersCaseApplication.class.getDeclaredMethod("main", String[].class);
-
-        assertThat(mainMethod.getReturnType()).isEqualTo(void.class);
-        assertThat(Modifier.isStatic(mainMethod.getModifiers())).isTrue();
+    void contextLoads() {
     }
 
 }
